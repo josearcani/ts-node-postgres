dc76c7b2e79a9bc70094341a600192e0f787a3ad
Juan Jose <juanjose.arcani2309@gmail.com>
2022-02-08 12:24:03 -0400

    refactor: simplify middlewares exports

---
+++ middlewares/index.ts
@@ -0,0 +1,9 @@
+import { validarCampos } from './validar-campos';
+import { validarJWT } from './validar-jwt';
+import { validarRol } from './validar-rol';
+
+export {
+  validarCampos,
+  validarJWT,
+  validarRol,
+}
