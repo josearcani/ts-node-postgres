091efa49d3ced6539fe274448c30085bb2a81e79
Juan Jose <juanjose.arcani2309@gmail.com>
2022-02-08 00:49:24 -0400

    sequelize: set tables for actividades, cursos y clases

---
+++ models/actividad.ts
@@ -0,0 +1,24 @@
+import { DataTypes } from 'sequelize';
+import db from '../db/connection';
+
+const Actividad = db.define('actividades', {
+  nombreActividad: {
+    type: DataTypes.STRING
+  },
+  descripcion: {
+    type: DataTypes.STRING
+  },
+  cursoId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'cursos',
+      key: 'id'
+    }
+  },
+},{
+  freezeTableName: true,
+});
+
+
+export default Actividad;
