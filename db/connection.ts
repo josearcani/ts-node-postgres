17ab56c2e1fbfe2766a16cb17755c674b4097381
Juan Jose <juanjose.arcani2309@gmail.com>
2022-03-17 23:47:42 -0400

    deploy: attempt one

--- db/connection.ts
+++ db/connection.ts
@@ -1,7 +1,12 @@
 import { Sequelize } from 'sequelize';
 
-const db = new Sequelize('node', 'testUser', '$Pass123', {
-    host: 'localhost',
+// const db = new Sequelize('node', 'testUser', '$Pass123', {
+//     host: 'localhost',
+//     dialect: 'postgres',
+//     // logging: false,
+// });
+const db = new Sequelize('dadb02k3sp1jh2', 'riomreuepzsnsc', 'c183f220564be229d2fa83f1449780f094e6e6d309955883d92bddacb1270ef7', {
+    host: 'ec2-34-236-34-103.compute-1.amazonaws.com',
     dialect: 'postgres',
     // logging: false,
 });
